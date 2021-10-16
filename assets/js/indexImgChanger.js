491732120e3325d58e1f7cfb261145c91c40588e
Fola Flor <dpcalfola@gmail.com>
2021-10-17 00:19:37 +0900

    ver.1.2.4_i
    JS : Random images ( on index.html )

---
+++ assets/js/indexImgChanger.js
@@ -0,0 +1,14 @@
+const images = [
+  "index_img_000.jpg",
+  "index_img_001.jpg",
+  "index_img_002.jpg",
+  "index_img_003.jpg",
+  "index_img_004.jpg",
+  "index_img_005.jpg",
+];
+
+const indexImg = document.getElementById("main_picture");
+
+const randomImg = images[Math.floor(Math.random() * images.length)];
+
+indexImg.src = `./assets/images/${randomImg}`;
