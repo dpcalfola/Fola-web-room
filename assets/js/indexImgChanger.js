4961d9a13838fe9e91a9d7dba7d9a06587d3ae76
Fola Flor <dpcalfola@gmail.com>
2021-10-17 14:59:53 +0900

    ver.1.2.412_i
    To correct image file error(4)

--- assets/js/indexImgChanger.js
+++ assets/js/indexImgChanger.js
@@ -1,10 +1,10 @@
 const images = [
-  "index_img_000.jpg",
   "index_img_001.jpg",
   "index_img_002.jpg",
   "index_img_003.jpg",
   "index_img_004.jpg",
   "index_img_005.jpg",
+  "index_img_006.jpg",
 ];
 
 const indexImg = document.getElementById("main_picture");
@@ -14,3 +14,4 @@ const randomImg = images[Math.floor(Math.random() * images.length)];
 indexImg.src = `./assets/images/${randomImg}`;
 
 console.log(randomImg);
+console.log(images.length);
